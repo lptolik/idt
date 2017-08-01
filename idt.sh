b92edfb112e0bb9a3074a9ad558aedea6496a3d9
A Sorokin <lptolik@gmail.com>
2017-08-01 20:05:53 +0900

    translit

---
+++ idt.sh
@@ -0,0 +1,82 @@
+#!/bin/bash
+
+# Usage info
+show_help() {
+cat << EOF
+Usage: ${0##*/} [-htv] [MESSAGE]...
+Create blog with header MESSAGE and timestamp.
+
+    -h          display this help and exit
+    -t			create blog template and open TextWrangler
+    -v			create blog template and open vim
+EOF
+}
+
+EDITOR='N'
+while getopts "htv:" opt; do
+	case $opt in
+		h)
+			show_help
+			exit 0
+			;;
+		v)  EDITOR=$opt
+			;;
+		t)  EDITOR=$opt
+			;;
+		*)
+			show_help >&2
+			exit 1
+			;;
+	esac
+done
+shift "$((OPTIND-1))"   # Discard the options and sentinel --
+
+# Everything that's left in "$@" is a non-option.  In our case, a FILE to process.
+
+fname=""
+string=""
+for a in "$@" # Loop over arguments
+do
+	if [[ "${a:0:1}" != "-" ]] # Ignore flags (first character is -)
+	then
+		if [[ "$string" != "" ]]
+		then
+			string+=" " # Delimeter
+			fname+="_" # Delimeter
+		fi
+		string+="$a"
+		fname+="$a"
+	fi
+done
+echo "$string"
+
+fname+=".txt"
+fname=`echo "${fname}" | tr '[A-Z]' '[a-z]'`
+
+DIR='/Users/lptolik/Documents/Projects/Plans/dokuwiki/idonethis'
+
+rDIR=$PWD
+cd $DIR
+
+if [[ $(git status -s) ]]
+then
+    echo "The idonethis directory is dirty. Please commit any pending changes."
+    exit 1;
+fi
+
+printf "====== $string ======\n\n\n" > $fname
+printf "%s\n" "---" "$(date '+%F %T')" " " "{{tag>IDT blog $(date +%Y) $(date +%B)}}" >> $fname
+
+case $EDITOR in
+	v)  vim "$fname"
+		;;
+	t)  otw "$fname"
+		;;
+	*)
+		;;
+esac
+git pull
+git add $fname
+git commit -m "' blog $(date '+%F %T')'"
+git push
+cd $rDIR
